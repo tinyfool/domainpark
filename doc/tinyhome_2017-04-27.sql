3191654a454edd63ee8f624710acfa508becd5be
HaoPeiqiang <tinyfool@gmail.com>
2017-04-27 16:52:39 +0800

    Add table sql

---
+++ doc/tinyhome_2017-04-27.sql
@@ -0,0 +1,59 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.16)
+# Database: tinyhome
+# Generation Time: 2017-04-27 08:50:53 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table channels
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `channels`;
+
+CREATE TABLE `channels` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `rssurl` text NOT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `url` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table contents
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `contents`;
+
+CREATE TABLE `contents` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `guid` text,
+  `url` text,
+  `title` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
